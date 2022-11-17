c36b73e542be1b7ea3c84f4e898e1240b8e69c0b
Ynewtime <chanfoc@outlook.com>
2022-11-17 14:57:45 +0300

    Support node proxy; Use .env.local

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import cron from 'node-cron';
 import prettier from 'prettier';
+import { bootstrap } from 'global-agent';
 
+proxySetup();
 require('dotenv').config();
 
 const COOKIE_FILE = path.join(__dirname, './cookie.json');
@@ -76,4 +78,66 @@ interface Daily {
       dailySongs: any[];
     };
   };
+}
+
+/**
+ * 设置 Node 代理
+ * @param {string} [proxy] 系统代理
+ * @param {string} [noProxy] 代理白名单
+ */
+export function proxySetup(proxy?: string, noProxy?: string) {
+  if (proxy) {
+    if (noProxy) process.env.GLOBAL_AGENT_NO_PROXY = noProxy;
+    process.env.GLOBAL_AGENT_HTTP_PROXY = proxy;
+    bootstrap();
+  } else {
+    // 看本地 SHELL 环境是否有配置代理
+    const {
+      NO_PROXY,
+      no_proxy,
+      HTTPS_PROXY,
+      https_proxy,
+      HTTP_PROXY,
+      http_proxy,
+      ALL_PROXY,
+      all_proxy,
+    } = process.env;
+
+    if (NO_PROXY || no_proxy) {
+      process.env.GLOBAL_AGENT_NO_PROXY = NO_PROXY || no_proxy;
+    }
+    if (
+      HTTPS_PROXY ||
+      https_proxy ||
+      HTTP_PROXY ||
+      http_proxy ||
+      ALL_PROXY ||
+      all_proxy
+    ) {
+      // 需要清除 process.env 中的代理配置
+      process.env.HTTP_PROXY = '';
+      process.env.http_proxy = '';
+      process.env.HTTPS_PROXY = '';
+      process.env.https_proxy = '';
+      process.env.ALL_PROXY = '';
+      process.env.all_proxy = '';
+      process.env.GLOBAL_AGENT_HTTP_PROXY =
+        HTTPS_PROXY ||
+        https_proxy ||
+        HTTP_PROXY ||
+        http_proxy ||
+        ALL_PROXY ||
+        all_proxy;
+      bootstrap();
+    } else {
+      // 没有的话，尝试从 .env 读取代理
+      // dotenv.config()
+      // const { NO_PROXY, PROXY } = process.env
+      // if (NO_PROXY) process.env.GLOBAL_AGENT_NO_PROXY = NO_PROXY
+      // if (PROXY) {
+      //   process.env.GLOBAL_AGENT_HTTP_PROXY = PROXY
+      //   bootstrap()
+      // }
+    }
+  }
 }
