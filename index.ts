448664f3a5b5852cf165586106e70b0d36ef85bc
龙崎流也 <Ynewtime@users.noreply.github.com>
2022-11-19 06:14:00 +0300

    Support node proxy; Use .env.local (#1)
    * Support node proxy; Use .env.local
    
    * Use dotenv-flow

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import fs from 'fs';
 import path from 'path';
 import cron from 'node-cron';
 import prettier from 'prettier';
+import { bootstrap } from 'global-agent';
 
-require('dotenv').config();
+proxySetup();
+require('dotenv-flow').config();
 
 const COOKIE_FILE = path.join(__dirname, './cookie.json');
 const REFRESH_OUT_DIR = path.join(__dirname, './refresh_out');
@@ -45,11 +47,14 @@ const run = async () => {
   }
 
   async function initialCookie() {
+    console.log('第一次登录，获取初始 cookie ...');
     const result = await login_cellphone({
       phone: process.env.N_PHONE,
       password: process.env.N_PASSWORD,
     });
-    console.log('第一次登录，获取初始 cookie ...');
+    if (result?.body?.msg) {
+      throw new Error(result.body.msg as string);
+    }
     fs.writeFileSync(COOKIE_FILE, JSON.stringify(result), 'utf-8');
   }
 };
@@ -76,4 +81,66 @@ interface Daily {
       dailySongs: any[];
     };
   };
+}
+
+/**
+ * 设置 Node 代理
+ * @param {string} [proxy] 系统代理
+ * @param {string} [noProxy] 代理白名单
+ */
+export function proxySetup(proxy?: string, noProxy?: string) {
+  if (proxy) {
+    if (noProxy) process.env.GLOBAL_AGENT_NO_PROXY = noProxy;
+    process.env.GLOBAL_AGENT_HTTP_PROXY = proxy;
+    bootstrap();
+  } else {
+    // 看本地 SHELL 环境是否有配置代理
+    const {
+      NO_PROXY,
+      no_proxy,
+      HTTPS_PROXY,
+      https_proxy,
+      HTTP_PROXY,
+      http_proxy,
+      ALL_PROXY,
+      all_proxy,
+    } = process.env;
+
+    if (NO_PROXY || no_proxy) {
+      process.env.GLOBAL_AGENT_NO_PROXY = NO_PROXY || no_proxy;
+    }
+    if (
+      HTTPS_PROXY ||
+      https_proxy ||
+      HTTP_PROXY ||
+      http_proxy ||
+      ALL_PROXY ||
+      all_proxy
+    ) {
+      // 需要清除 process.env 中的代理配置
+      process.env.HTTP_PROXY = '';
+      process.env.http_proxy = '';
+      process.env.HTTPS_PROXY = '';
+      process.env.https_proxy = '';
+      process.env.ALL_PROXY = '';
+      process.env.all_proxy = '';
+      process.env.GLOBAL_AGENT_HTTP_PROXY =
+        HTTPS_PROXY ||
+        https_proxy ||
+        HTTP_PROXY ||
+        http_proxy ||
+        ALL_PROXY ||
+        all_proxy;
+      bootstrap();
+    } else {
+      // 本地 SHELL 环境没有配置代理的话，尝试从 .env.local 读取代理
+      // dotenv.config()
+      // const { NO_PROXY, PROXY } = process.env
+      // if (NO_PROXY) process.env.GLOBAL_AGENT_NO_PROXY = NO_PROXY
+      // if (PROXY) {
+      //   process.env.GLOBAL_AGENT_HTTP_PROXY = PROXY
+      //   bootstrap()
+      // }
+    }
+  }
 }
